1cb8a53426ce29b9d747bbfb529553c0011999f2
prazzdev <prazzid31@gmail.com>
2024-08-13 16:06:30 +0700

    modified:   astro.config.mjs 	modified:   package-lock.json 	modified:   package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
   }),
 });
